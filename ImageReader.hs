b60321f8d434f3c4b1c48bf82bead3229aeca146
Sebastian Korten <Sebastian.Korten@neuland-bfi.de>
2012-05-25 19:22:27 +0200

    Read JPG, Png and Bitmap-Images

--- ImageReader.hs
+++ ImageReader.hs
@@ -1,31 +1,30 @@
 module ImageReader (
-                    getJpegData
+                     getImageData
                    ) where
 import System.Environment (getArgs)
 import Codec.Picture
 import Codec.Picture.Types
-import qualified Data.ByteString as B
 
-getJpegData :: FilePath -> IO (Maybe (Image PixelRGB8))
-getJpegData filePath = do
-    file <- B.readFile filePath
-    rez <- catch (return $ decodeJpeg file)
-                 (\err -> return $ Left (show err))
+getImageData :: FilePath -> IO (Maybe (Image PixelRGB8))
+getImageData filePath = do
+    rez <- readImage filePath
     case rez of
-        Left err -> (putStr $ "\n(X) JPEG loading error: (" ++ filePath ++ ")" ++ err) >> return Nothing
+        Left err -> putStr ("\n(X) loading error: (" ++ filePath ++ ")" ++ err) >> return Nothing
         Right (ImageYCbCr8 img) -> return $ Just $ convertImage img
-        Right (ImageY8 img) -> return $ Just $ promoteImage img
-        Right _ -> (putStr $ "\n(X) JPEG loading error: (" ++ filePath ++ ")") >> return Nothing
+        Right (ImageY8 img)     -> return $ Just $ promoteImage img
+        Right (ImageYA8 img)    -> return $ Just $ promoteImage img
+        Right (ImageRGB8 img)   -> return $ Just $ promoteImage img
+        Right (ImageRGBA8 img)  -> return $ Just $ convertImage $ dropAlphaLayer img
 
 -- inspectify :: Maybe (Image PixelRGB8) -> String
--- inspectify Nothing  = "Nix zu sehen..."
+-- inspectify Nothing  = "Nothing to see here..."
 -- inspectify (Just i) = "Width: " ++ show (imageWidth i) ++ ", Height: " ++ show (imageHeight i)
 
 -- main :: IO ()
 -- main = do
 --         args <- getArgs
---         if length args == 0
---         then putStrLn "Please specify a jpg as a parameter"
+--         if null args
+--         then putStrLn "Please specify an image as a parameter"
 --         else do
---                 daten <- getJpegData $ head args
+--                 daten <- getImageData $ head args
 --                 putStrLn $ inspectify daten
